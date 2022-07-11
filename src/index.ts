217099bd8cbb1f8b8d19f459609ca02312cf9657
Ren <rpaczkowski@gmail.com>
2022-07-11 13:21:59 -0400

    Got basics set up

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Listening on port: ${port}.`));
+
+const routes = express.Router();
+
+export default routes;
