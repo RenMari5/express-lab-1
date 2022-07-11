cbece57edb191f41a294f51045f0748408d8222f
Ren <rpaczkowski@gmail.com>
2022-07-11 16:58:32 -0400

    Finished the rest of the requests

--- src/routes/cart-items.ts
+++ src/routes/cart-items.ts
@@ -3,14 +3,39 @@ import { v4 as uuidv4 } from "uuid";
 import { cartItems } from "../data/cart";
 import { Item } from "../types/item.types";
 
-export const cartRoutes = express.Router();
+export const cartRouter = express.Router();
 // export default routes;
 
-cartRoutes.get("/cart-items", (req, res) => {
+cartRouter.get("/cart-items", (req, res) => {
   res.json(cartItems);
 });
 
-cartRoutes.get("/cart-items:id", (req, res) => {
+cartRouter.get("/cart-items:id", (req, res) => {
   const item = cartItems.find((itemObject) => itemObject.id === req.params.id);
   res.json(item);
+});
+
+cartRouter.post("/", (req, res) => {
+  const newItem: Item = { id: uuidv4(), ...req.body };
+  cartItems.push(newItem);
+  res.json(newItem);
+});
+
+cartRouter.put("/:id", (req, res) => {
+  const item = cartItems.find(
+    (itemObject) => itemObject.id === req.params.id
+  ) as Item;
+  const itemIndex = cartItems.findIndex((item) => item.id === req.params.id);
+
+  const updatedItem = { ...item, ...req.body };
+
+  cartItems.splice(itemIndex, 1, updatedItem);
+
+  res.json(updatedItem);
+});
+
+cartRouter.delete("/:id", (req, res) => {
+  const itemIndex = cartItems.findIndex((item) => item.id === req.params.id);
+  cartItems.splice(itemIndex, 1);
+  res.json({ message: "Item deleted" });
 });
